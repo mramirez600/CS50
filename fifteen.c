74f554ea7a53aaa716dff373d36af915fdc3a922
Marvin Ramirez <eldo@hotmail.com>
2017-07-11 12:50:54 -0400

    Create fifteen.c
    The Game of Fifteen is a puzzle played on a square, two-dimensional board with numbered tiles that slide. 
    
    Implement the following functions - 
    1. Implement init.
    2. Implement draw.
    3. Implement move.
    4. Implement won.

---
+++ fifteen.c
@@ -0,0 +1,277 @@
+/**
+ * fifteen.c
+ *
+ * Implements Game of Fifteen (generalized to d x d).
+ *
+ * Usage: fifteen d
+ *
+ * whereby the board's dimensions are to be d x d,
+ * where d must be in [DIM_MIN,DIM_MAX]
+ *
+ * Note that usleep is obsolete, but it offers more granularity than
+ * sleep and is simpler to use than nanosleep; `man usleep` for more.
+ */
+ 
+#define _XOPEN_SOURCE 500
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// constants
+#define DIM_MIN 3
+#define DIM_MAX 9
+
+// board
+int board[DIM_MAX][DIM_MAX];
+
+// dimensions
+int d;
+
+// prototypes
+void clear(void);
+void greet(void);
+void init(void);
+void draw(void);
+bool move(int tile);
+bool won(void);
+
+int main(int argc, string argv[])
+{
+    // ensure proper usage
+    if (argc != 2)
+    {
+        printf("Usage: fifteen d\n");
+        return 1;
+    }
+
+    // ensure valid dimensions
+    d = atoi(argv[1]);
+    if (d < DIM_MIN || d > DIM_MAX)
+    {
+        printf("Board must be between %i x %i and %i x %i, inclusive.\n",
+            DIM_MIN, DIM_MIN, DIM_MAX, DIM_MAX);
+        return 2;
+    }
+
+    // open log
+    FILE *file = fopen("log.txt", "w");
+    if (file == NULL)
+    {
+        return 3;
+    }
+
+    // greet user with instructions
+    greet();
+
+    // initialize the board
+    init();
+
+    // accept moves until game is won
+    while (true)
+    {
+        // clear the screen
+        clear();
+
+        // draw the current state of the board
+        draw();
+
+        // log the current state of the board (for testing)
+        for (int i = 0; i < d; i++)
+        {
+            for (int j = 0; j < d; j++)
+            {
+                fprintf(file, "%i", board[i][j]);
+                if (j < d - 1)
+                {
+                    fprintf(file, "|");
+                }
+            }
+            fprintf(file, "\n");
+        }
+        fflush(file);
+
+        // check for win
+        if (won())
+        {
+            printf("ftw!\n");
+            break;
+        }
+
+        // prompt for move
+        printf("Tile to move: ");
+        int tile = get_int();
+        
+        // quit if user inputs 0 (for testing)
+        if (tile == 0)
+        {
+            break;
+        }
+
+        // log move (for testing)
+        fprintf(file, "%i\n", tile);
+        fflush(file);
+
+        // move if possible, else report illegality
+        if (!move(tile))
+        {
+            printf("\nIllegal move.\n");
+            usleep(500000);
+        }
+
+        // sleep thread for animation's sake
+        usleep(500000);
+    }
+    
+    // close log
+    fclose(file);
+
+    // success
+    return 0;
+}
+
+/**
+ * Clears screen using ANSI escape sequences.
+ */
+void clear(void)
+{
+    printf("\033[2J");
+    printf("\033[%d;%dH", 0, 0);
+}
+
+/**
+ * Greets player.
+ */
+void greet(void)
+{
+    clear();
+    printf("WELCOME TO GAME OF FIFTEEN\n");
+    usleep(2000000);
+}
+
+/**
+ * Initializes the game's board with tiles numbered 1 through d*d - 1
+ * (i.e., fills 2D array with values but does not actually print them).  
+ */
+void init(void)
+{
+    // TODO
+    int totalTiles = d * d;
+    
+    //loop through and create required tiles
+    for (int i = 0; i < d; i++) {
+        for (int j = 0; j < d; j++) {
+           board[i][j] = --totalTiles;
+        }
+    }
+    //swap tile #1 and #2 depending on dimension selected
+      if ((d*d)%2 == 0) {
+        board[d - 1][d - 3] = 1;
+        board[d - 1][d - 2] = 2;
+    }
+            
+}
+
+/**
+ * Prints the board in its current state.
+ */
+void draw(void)
+{
+    // TODO
+    //print board from tiles created in init above
+    for (int i = 0; i < d; i++) {
+       
+        for (int j = 0; j < d; j++) {
+            
+            if (board[i][j] == 0) {
+                printf("  _");
+            }
+            else 
+            {
+                printf("%3i", board[i][j]);
+            }
+        }
+        printf("\n\n");
+    }
+    
+}
+
+/**
+ * If tile borders empty space, moves tile and returns true, else
+ * returns false. 
+ */
+bool move(int tile)
+{
+    // TODO
+    //validate tile
+    if (tile > d*d-1 || tile < 1) {
+        return false;
+    }
+    
+    int row = 0;
+    int col = 0;
+    //search for tile on board
+    for (int i = 0; i < d; i++) {
+        for (int j = 0; j < d; j++)
+        {
+            if (board[i][j] == tile)
+            {
+                row = i;
+                col = j;
+            }
+        }
+    }
+
+    // Checks for the "  _" around selected tile 
+    if (row - 1 >= 0 && board[row - 1][col] == 0)
+    {
+        board[row - 1][col] = board[row][col];
+        board[row][col] = 0;
+        return true;
+    }
+    else if (row + 1 < d && board[row + 1][col] == 0)
+    {
+        board[row + 1][col] = board[row][col];
+        board[row][col] = 0;
+        return true;
+    }
+    else if (col - 1 >= 0 && board[row][col - 1] == 0)
+    {
+        board[row][col - 1] = board[row][col];
+        board[row][col] = 0;
+        return true;
+    }
+    else if (col + 1 < d && board[row][col + 1] == 0)
+    {
+        board[row][col + 1] = board[row][col];
+        board[row][col] = 0;
+        return true;
+    }
+    
+    return false;
+
+}
+
+/**
+ * Returns true if game is won (i.e., board is in winning configuration), 
+ * else false.
+ */
+bool won(void)
+{
+    // TODO
+
+    int win[d*d][d*d];
+    
+    //check order of tiles
+    for(int i=0;i<d;i++) {
+        for(int j=0; j<d; j++){        
+             win[i][j]= (i*d)+(j+1);
+
+             if((win[i][j] !=d*d) && (board[i][j] != win[i][j]) ) {
+                return false;
+             }
+        }
+    }        
+              return true;
+}
